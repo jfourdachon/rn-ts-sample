abb25e0df96c533b77c020ea2943c5f3655c5b4e
jfourdachon <jfourdachon@yahoo.com>
2021-02-19 21:35:04 +0100

    image from url

--- screens/GameOverScreen.tsx
+++ screens/GameOverScreen.tsx
@@ -13,7 +13,8 @@ const GameOverScreen = ({ roundsCount, userChoice, startNewGame }: IProps) => {
         <View style={styles.screen}>
             <Text style={style.title}>The Game is over!</Text>
             <View style={styles.imageContainer}>
-                <Image source={require('../assets/images/success.png')} style={styles.image} resizeMode="cover" />
+                {/* <Image source={require('../assets/images/success.png')} style={styles.image} resizeMode="cover" /> */}
+                <Image source={{uri: 'https://www.oddizzi.com/wp-content/uploads/2011/01/img-woman-on-summit_big.jpg'}} style={styles.image} resizeMode="stretch" fadeDuration={1000} />
             </View>
             <Text style={style.body}>Number of rounds: {roundsCount}</Text>
             <Text style={style.body}>Number was: {userChoice}</Text>
@@ -38,6 +39,7 @@ const styles = StyleSheet.create({
         marginVertical: 20
     },
     image: {
+        // always set width + height on images with url
         width: '100%',
         height: '100%',
     }
